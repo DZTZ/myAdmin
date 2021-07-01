0a78cbc3826d175095654bb9d531927762037765
DZTZ <1310167434@qq.com>
2021-07-01 14:16:25 +0800

    cdn

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,8 @@ let cdn = {
     "https://cdn.jsdelivr.net/npm/vue-router@3.4.9/dist/vue-router.min.js",
     "https://cdn.jsdelivr.net/npm/vuex@3.6.0/dist/vuex.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js",
-    "https://cdn.bootcdn.net/ajax/libs/ant-design-vue/1.7.2/antd.min.js"
+    "https://dztz.github.io/cdn/antd.min.js"
+    // "https://cdn.bootcdn.net/ajax/libs/ant-design-vue/1.7.2/antd.min.js"
   ]
 };
 module.exports = {
